820d11751346c79a06c1297ff204822700eb373d
aditya-user <adiunni74@gmail.com>
2022-02-26 23:11:34 +0530

    fix

--- app/src/main/java/com/example/venteran/Registration.java
+++ app/src/main/java/com/example/venteran/Registration.java
@@ -3,12 +3,36 @@ package com.example.venteran;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
 
 public class Registration extends AppCompatActivity {
+
+    EditText mgetphonenumber;
+    android.widget.Button mregister;
+    CountryCodePicker mcountrycodepicker;
+    String countrycode;
+    String phonenumber;
+
+    FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofmain;
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String codesent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mcountrycodepicker=findViewById(R.id.ccp);
+        mregister=findViewById(R.id.regbtn);
+        mgetphonenumber=findViewById(R.id.reg_mobile);
+        mprogressbarofmain=findViewById(R.id.progreesbarforreg);
+
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 }
