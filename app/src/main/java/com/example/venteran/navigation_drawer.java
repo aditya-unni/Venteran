46c879ddc843c1bda76a3f44cc596d7e3d43ef76
DeKu420 <kevinjohn0805@gmail.com>
2022-03-12 03:50:58 +0530

    status done right

--- app/src/main/java/com/example/venteran/navigation_drawer.java
+++ app/src/main/java/com/example/venteran/navigation_drawer.java
@@ -161,6 +161,20 @@ public class navigation_drawer extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        DocumentReference documentReference = firebaseFirestore.collection("Users").document(firebaseAuth.getUid());
+        documentReference.update("status", "Offline").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                //user is offline
+            }
+        });
+    }
+
+
 
 
 //    @Override
